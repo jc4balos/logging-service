a87fea783ab4cce5281b86ad38084dee3f436af0
Jc Abalos <jc4balos@gmail.com>
2024-06-03 15:51:51 +0800

    added loggers for debugging

--- src/main/java/com/jc4balos/logging_service/model/Component.java
+++ src/main/java/com/jc4balos/logging_service/model/Component.java
@@ -12,7 +12,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "logs")
+@Table(name = "component")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- src/test/java/com/jc4balos/logging_service/LoggingServiceApplicationTests.java
+++ src/test/java/com/jc4balos/logging_service/LoggingServiceApplicationTests.java
@@ -8,6 +8,7 @@ class LoggingServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
