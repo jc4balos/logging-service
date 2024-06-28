7e7b5bea9e292316d325281158dcfb75c641ac49
Jc Abalos <jc4balos@gmail.com>
2024-06-28 09:24:27 +0800

    added component rep[ository

--- src/main/java/com/jc4balos/logging_service/repository/ComponentRepository.java
+++ src/main/java/com/jc4balos/logging_service/repository/ComponentRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.jc4balos.logging_service.model.ServiceComponent;
 
 public interface ComponentRepository extends JpaRepository<ServiceComponent, Long> {
+
 }
