3cff4bd851dee73ef054dedfe4edcba834aeec40
Jc Abalos <jc4balos@gmail.com>
2024-06-29 11:52:50 +0800

    added logsmapper

---
+++ src/main/java/com/jc4balos/logging_service/mapper/LogsMapper.java
@@ -0,0 +1,46 @@
+package com.jc4balos.logging_service.mapper;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.jc4balos.logging_service.dto.logs.NewLogDto;
+import com.jc4balos.logging_service.model.Logs;
+import com.jc4balos.logging_service.model.ServiceComponent;
+import com.jc4balos.logging_service.repository.ServiceComponentRepository;
+import com.jc4balos.logging_service.service.component.v1.ComponentServiceImpl;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class LogsMapper {
+
+    @Autowired
+    private Logs logs;
+
+    @Autowired
+    private ServiceComponent serviceComponent;
+
+    private final ServiceComponentRepository serviceComponentRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(ComponentServiceImpl.class);
+
+    public Logs newLog(NewLogDto newLogDto) {
+        Optional<ServiceComponent> logServiceComponent = serviceComponentRepository
+                .findById(newLogDto.getComponentId());
+        if (!logServiceComponent.isPresent()) {
+            String responseMessage = "Component not found.";
+            logger.error(responseMessage);
+            throw new RuntimeException(responseMessage);
+        }
+        logs.setServiceComponent(logServiceComponent.get());
+        logs.setEvent(newLogDto.getEventMessage());
+        logs.setUserId(newLogDto.getUserId());
+        return logs;
+    }
+
+}
