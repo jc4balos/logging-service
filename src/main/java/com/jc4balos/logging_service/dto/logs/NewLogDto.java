e5bac61b38525478c33e89e4dddfa585ed030d99
Jc Abalos <jc4balos@gmail.com>
2024-07-02 14:04:35 +0800

    added c

--- src/main/java/com/jc4balos/logging_service/dto/logs/NewLogDto.java
+++ src/main/java/com/jc4balos/logging_service/dto/logs/NewLogDto.java
@@ -1,9 +1,12 @@
 package com.jc4balos.logging_service.dto.logs;
 
+import org.springframework.stereotype.Component;
+
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
+@Component
 public class NewLogDto {
 
     @NotEmpty(message = "Component is required.")
--- src/main/java/com/jc4balos/logging_service/dto/logs/ViewLogDto.java
+++ src/main/java/com/jc4balos/logging_service/dto/logs/ViewLogDto.java
@@ -2,9 +2,12 @@ package com.jc4balos.logging_service.dto.logs;
 
 import java.time.LocalDateTime;
 
+import org.springframework.stereotype.Component;
+
 import lombok.Data;
 
 @Data
+@Component
 public class ViewLogDto {
 
     private String userName;
